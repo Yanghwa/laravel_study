00774602445a09b4609eb69e0ca06e6ab62bd223
junghwan.yang <junghwan.yang@adaware.com>
2017-09-11 16:31:00 -0400

    view change for master layout

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -2,13 +2,19 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="X-UA_Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
   <title>Laravel 5 Essential</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <link rel="stylesheet" href="{{elixir('css/app.css')}}">
+  @yield('style')
 </head>
 <body>
+  <div class="content">
+    @yield('content')
+    @include('footer')
 
-  @yield('content')
-
+  <script src="{{elixir('js/app.js')}}"></script>
+  @yield('script')
+  
 </body>
-@include('footer')
 </html>
