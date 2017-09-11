db3e2b0b226677cbff5f07e3ce7aacbb3b536261
junghwan.yang <junghwan.yang@adaware.com>
2017-09-11 15:14:34 -0400

    add cache to doc controller for loading pages

--- app/Http/Controllers/DocumentsController.php
+++ app/Http/Controllers/DocumentsController.php
@@ -14,12 +14,23 @@ class DocumentsController extends Controller
         $this->document = $document;    
     }
 
-    public function show($file = null) {
-        return view('documents.index', [
-            'index' => markdown($this->document->get()),
-            'content' => markdown($this->document->get($file ?: 'testing.md'))
-        ]);
-    }
+    // public function show($file = null) {
+    //     return view('documents.index', [
+    //         'index' => markdown($this->document->get()),
+    //         'content' => markdown($this->document->get($file ?: 'testing.md'))
+    //     ]);
+    // }
 
+    public function show($file = 'testing.md') {
+        $index = \Cache::remember('documents.index', 120, function() {
+            return markdown($this->document->get());
+        });
+
+        $content = \Cache::remember('documents.{$file}', 120, function() use ($file) {
+            return markdown($this->document->get($file));
+        });
+
+        return view('documents.index', compact('index', 'content'));
+    }
 
 }
